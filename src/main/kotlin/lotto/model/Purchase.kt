f5c67b81c8e50b8626ab651c3a3ef635301cd30c
cucumber99 <dlsguq1@gmail.com>
2024-05-08 13:49:00 +0900

    feat: Purchase 모델 구현

--- src/main/kotlin/lotto/model/Purchase.kt
+++ src/main/kotlin/lotto/model/Purchase.kt
@@ -2,8 +2,16 @@ package lotto.model
 
 import lotto.util.Validator.validateAmount
 
-class Ticket(private val amount: Int) {
+class Purchase(private val amount: Int) {
+    companion object {
+        private const val LOTTO_PRICE = 1000
+    }
+
     init {
         validateAmount(amount)
+    }
+
+    fun calculatePurchaseCount(): Int {
+        return amount / LOTTO_PRICE
     }
 }
