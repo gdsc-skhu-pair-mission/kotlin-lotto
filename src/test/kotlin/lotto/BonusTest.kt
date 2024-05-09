d99ee5ecf0dec50c91c7a1011ad867a4b575be64
cucumber99 <dlsguq1@gmail.com>
2024-05-09 14:13:41 +0900

    test: Bonus 모델 테스트 수정

--- src/test/kotlin/lotto/BonusTest.kt
+++ src/test/kotlin/lotto/BonusTest.kt
@@ -1,16 +1,19 @@
 package lotto
 
 import lotto.model.Bonus
+import lotto.model.Lotto
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
 
 class BonusTest {
+    private val lotto = Lotto(listOf(1,2,3,4,5,6))
+
     @Test
     @DisplayName("보너스 번호는 1부터 45 사이여야 한다.")
     fun should_ThrowException_When_OutOfRange() {
         assertThrows<IllegalArgumentException> {
-            Bonus(46)
+            Bonus(lotto, 46)
         }
     }
 
@@ -18,8 +21,8 @@ class BonusTest {
     @DisplayName("보너스 번호는 당첨 번호와 중복되지 않아야 한다.")
     fun should_ThrowException_When_Duplicate() {
         assertThrows<IllegalArgumentException> {
-            val bonus = Bonus(1)
-            bonus.checkDuplicateWithLotto(listOf(1,2,3,4,5,6))
+            val bonus = Bonus(lotto, 1)
+            bonus.checkDuplicateWithLotto()
         }
     }
 }
