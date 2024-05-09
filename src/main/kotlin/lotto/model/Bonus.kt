01e5bb0b188009dc761fc7406cd849a8393ea671
cucumber99 <dlsguq1@gmail.com>
2024-05-09 14:13:16 +0900

    refactor: Bonus 모델 수정

--- src/main/kotlin/lotto/model/Bonus.kt
+++ src/main/kotlin/lotto/model/Bonus.kt
@@ -3,12 +3,15 @@ package lotto.model
 import lotto.util.Validator.validateBonusRange
 import lotto.util.Validator.validateLottoBonusDuplicate
 
-class Bonus (private val bonusNumber: Int){
+class Bonus (
+    private val lotto: Lotto,
+    private val bonusNumber: Int
+) {
     init {
         validateBonusRange(bonusNumber)
     }
 
-    fun checkDuplicateWithLotto(numbers: List<Int>) {
-        validateLottoBonusDuplicate(numbers, bonusNumber)
+    fun checkDuplicateWithLotto() {
+        validateLottoBonusDuplicate(lotto.getWinningNumbers(), bonusNumber)
     }
 }
