423293058fc7a0a2fbba573c9ae4397cd91105f8
NamGung Hyemin <ng040115@gmail.com>
2024-05-16 17:40:55 +0900

    📝 Ticket 클래스 작성

---
+++ src/main/kotlin/lotto/model/Ticket.kt
@@ -0,0 +1,16 @@
+package lotto.model
+
+import camp.nextstep.edu.missionutils.Randoms
+import kotlin.random.Random
+
+class Ticket{
+    companion object {
+        const val MIN_NUMBER = 1
+        const val MAX_NUMBER = 45
+        const val LOTTO_SIZE = 6
+    }
+    // 로또 숫자를 생성하는 메소드 ( 리스트로 반환)
+     fun generateLottoNumber(): List<Int>{
+        return Randoms.pickUniqueNumbersInRange(MIN_NUMBER, MAX_NUMBER, LOTTO_SIZE).sorted()
+    }
+}
